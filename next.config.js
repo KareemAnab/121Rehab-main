28d99fc1e7f5f803252096d60cec9634564cbade
KareemAnab <Kareem@tex-crete.com>
2026-01-18 16:26:02 -0800

    Add privacy policy, terms of service, and final site updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "121rehabcorp.com" },
+      { protocol: "https", hostname: "www.121rehabcorp.com" },
+    ],
+  },
 };
 
 module.exports = nextConfig;
